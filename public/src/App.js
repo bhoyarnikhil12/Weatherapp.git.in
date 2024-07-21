2473030dd29d0a19cafcc5503dcb5dd33af98851
bhoyarnikhil12 <140892322+bhoyarnikhil12@users.noreply.github.com>
2024-07-21 19:55:12 +0530

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import visibility from "./../src/visibility.png";
+import wind from "./cloudy-day.png";
+import thunder from "./thunder.png";
+import rain from "./rain.png";
+export default function App() {
+const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("Pune");
+  const [weatherDescription, setWeatherDescription] = useState("");
+  async function loadWeatherData() {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f652964084c552e8c0492237a3fabd9c`)
+      setWeatherData(response.data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    loadWeatherData();
+  }, [city])
+  useEffect(() => {
+    setWeatherDescription(
+      `${weatherData?.weather?.[0]?.main} (${weatherData?.weather?.[0]?.description})`)
+  }, [weatherData])
+
+  return (
+    <div className="app">
+
+      <h1 className='text'>Weather App</h1>
+      {/* <h2 className='caption '>"Weather.. whenever.. whenever...!"</h2> */}
+
+      <input type='text' className='box' value={city} onChange={(e) => {
+        setCity(e.target.value);
+      }} />
+
+      <div className='bg-color'>
+        <p className='city'>City : {weatherData?.name}</p>
+        <p className='temp'>Temp:<br/><span className='font'> {(weatherData?.main?.temp - 273).toFixed(2)}°C</span></p>
+      </div>
+
+
+      <div className='flex'>
+        <div className='visibility'>
+          <p className='center'>Humidity <br />
+            <img src={visibility} className='img' /><br />
+
+            {weatherData?.main?.humidity} %
+          </p>
+        </div>
+
+        <div className='visibility'>
+          <p className='center'>Visibility <br />
+            <img src={thunder} className='img' /><br />{weatherData?.visibility}
+            &nbsp;M</p>
+        </div>
+
+        <div className='visibility'>
+          <p className='center'>Wind <br />
+            <img src={wind} className='img' /><br /> {weatherData?.wind?.speed} km/hr </p>
+        </div>
+      </div>
+      </div>
+
+
+
+      );
+}
